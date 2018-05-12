a4fdad8337ad800787493114b54593d7d25dad58
jordanhudgens <jordan@devcamp.com>
2018-05-11 23:51:26 -0500

    Built out initial directives and toggle functionality

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 new Vue({
   el: "#app",
   data: {
-    title: "Daily Smarty Branding Details"
+    title: "Daily Smarty Branding Details",
+    content: "Welcome to the branding page",
+    instagram: "https://www.instagram.com/dailysmarty/",
+    detailClasses: ["style-details-text", "blue_text"],
+    showImg: false
+  },
+  methods: {
+    greeting() {
+      return this.content;
+    },
+    toggleImg() {
+      console.log("toggleImg clicked");
+
+      if (this.showImg) {
+        this.showImg = false;
+      } else {
+        this.showImg = true;
+      }
+    }
   }
 });
